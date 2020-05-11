1a08d4ccf4176f719c7c582890fa0a7a74f3a4ff
mariana-kikloki <mariana_fernandes_27@hotmail.com>
2020-05-11 12:03:40 +0100

    final aula 11

--- TPraticas/Aula11/safe.py
+++ TPraticas/Aula11/safe.py
@@ -1,8 +1,8 @@
 import datetime
 
-def getName(name="o nome",size_min=3,size_max=64):
+def getName(size_min=3,size_max=64):
     while True:
-        inp = input("Introduza "+name+":")
+        inp = input("Introduza o nome: ")
         if(len(inp)>=size_min and (size_max<size_min or len(inp)<=size_max)):
             if 0<sum(list(map(lambda x:0 if x.isalpha()else 1,inp.split(" ")))):
                 print("Caracteres Inválidos!")
@@ -20,7 +20,7 @@ def getNum(name="um número",size=0,r_type=int,decimals=2):
 
             k = False
             if(isinstance(res,float) and (not('.' in inp) or len(inp.split(".")[1])>decimals)):
-                print("Apenas pode ter",decimals,"casas decimais!")
+                print("É obrigatório ter casas decimais, no máximo",decimals,"casas decimais!")
                 k = True
 
             if(not k and size>0 and len(inp)!=size):
@@ -31,7 +31,7 @@ def getNum(name="um número",size=0,r_type=int,decimals=2):
             print("Caracteres Inválidos!")
     return res
 
-def getDate(name="a data"):
+def getDate(name="a data de nascimento"):
     while True:
         try:
             inp = input("Introduza "+name+" (DD-MM-AAAA): ")
@@ -45,14 +45,16 @@ if __name__ == "__main__":
     nic   = getNum("NIC",8)
     cc    = getNum("Cartão de Crédito",16)
     cvv   = getNum("CVC/CVV", 3)
+    validade = getDate("a validade do cartão")
     nome  = getName()
-    valor = getNum("Valor",r_type=float)
     data  = getDate()
+    valor = getNum("Valor",r_type=float)
     
     print( "NIF:"  , nif  )
     print( "NIC:"  , nic  )
     print( "CC:"   , cc   )
     print( "CVV:"  , cvv  )
+    print( "VALIDADE:" , validade )
     print( "NOME:" , nome )
+    print( "DATA DE NASCIMENTO:" , data )
     print( "VALOR:", valor)
-    print( "DATA:" , data )
